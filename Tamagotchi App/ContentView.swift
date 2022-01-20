b4c26eab647220d72e8343dd548aadc2e5a5cc1a
Krystian Shanahan <shanahan.k@etoncollege.org.uk>
2022-01-20 11:48:39 +0000

    Initial Commit

---
+++ Tamagotchi App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tamagotchi App
+//
+//  Created by Shanahan, Krystian (JD) on 20/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tamagotchi App/Tamagotchi_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tamagotchi_AppApp.swift
+//  Tamagotchi App
+//
+//  Created by Shanahan, Krystian (JD) on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tamagotchi_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
